4250410596df901ba12d02ea187b63ced1dbcceb
fintonius <fintonius.mcginty@gmail.com>
2023-12-13 15:18:59 +0000

    adding ability for discord to add track based on chatGPT suggestion to user playlist
    relates #16

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
-require('dotenv/config');
+import 'dotenv/config';
 // Will give us access to all the .env variables
 
-const { Client } = require('discord.js');
-const { OpenAI } = require('openai');
+import { Client } from 'discord.js';
+import { OpenAI } from 'openai';
+import axios from 'axios';
+// the axios library in Node.js to make HTTP requests
+
+let address = '';
+let songSuggestion = {};
+const clientId = 'b9187564d05d46e891a197b24dc20983';
+const clientSecret = process.env.SPOTKEY;
+const trackId = '6NPVjNh8Jhru9xOmyQigds';
+const playlistId = '3cKZDGGHg6r9pAiwIOXGpZ';
 
 const client = new Client({
     intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
@@ -36,7 +45,7 @@ client.on('messageCreate', async (message) => {
 
     conversation.push({
         role: 'system',
-        content: 'Chat GPT is a friendly chatbot.'
+        content: 'Chat GPT is a friendly chatbot.',
     })
 
     let prevMessages = await message.channel.messages.fetch({limit: 10 });
@@ -70,6 +79,8 @@ client.on('messageCreate', async (message) => {
     const response = await openai.chat.completions.create({
         model: 'gpt-4',
         messages: conversation,
+        // prompt: 'format your answers as a JSON object with the keys title, artist, genre. format the link as a json object and separate the track ID number, which comes at the end of the url after track, into its own key data pair'
+
     })
     .catch((error) => console.error('OpenAI Error:\n', error));
 
@@ -81,6 +92,9 @@ client.on('messageCreate', async (message) => {
     }
 
     const responseMessage = response.choices[0].message.content;
+    songSuggestion.test = response.choices[0].message.content;
+    console.log('fintan testing', songSuggestion.test)
+    // addTrackToPlaylist();
     const chunkSizeLimit = 2000;
 
     for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
@@ -91,3 +105,171 @@ client.on('messageCreate', async (message) => {
 });
 
 client.login(process.env.TOKEN);
+
+
+/////////////////
+// SPOTIFY APP ACCESS
+/////////////////
+
+import express from 'express';
+import querystring from 'querystring';
+// import axios from 'axios';
+import { DateTime } from 'luxon';
+// import 'dotenv/config';
+
+const app = express();
+app.use(express.json());
+
+const CLIENT_ID = '2c52abffbc8742d2b5441acdc4aec18d';
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = 'http://localhost:8888/callback';
+
+const AUTH_URL = 'https://accounts.spotify.com/authorize';
+const TOKEN_URL = 'https://accounts.spotify.com/api/token';
+const API_BASE_URL = 'https://api.spotify.com/v1/';
+
+let session = {};
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the Spotify app <a href='/login'>Login</a>");
+});
+
+app.get('/login', (req, res) => {
+  const scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+
+  const params = {
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    scope: scope,
+    redirect_uri: REDIRECT_URI,
+    show_dialog: true,
+  };
+
+  const authUrl = `${AUTH_URL}?${querystring.stringify(params)}`;
+
+  res.redirect(authUrl);
+});
+
+app.get('/callback', async (req, res) => {
+  if ('error' in req.query) {
+    return res.json({ error: req.query.error });
+  }
+
+  if ('code' in req.query) {
+    const reqBody = {
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      redirect_uri: REDIRECT_URI,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+    };
+
+    try {
+      const response = await axios.post(TOKEN_URL, querystring.stringify(reqBody), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      });
+
+      const tokenInfo = response.data;
+
+
+      // Store token information in session
+      session.access_token = tokenInfo.access_token;
+      session.refresh_token = tokenInfo.refresh_token;
+      session.expires_at = DateTime.now().toSeconds() + tokenInfo.expires_in;
+
+      // Log the token information and session after retrieval
+      // console.log('Token Info:', tokenInfo);
+      // console.log('Session after token retrieval:', session);
+
+      return res.redirect('/playlists');
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to retrieve tokens' });
+    }
+  }
+});
+
+app.get('/playlists', async (req, res) => {
+  // console.log('Session before accessing playlists:', session);
+  if (!session.access_token) {
+    return res.redirect('/login');
+  }
+
+  if (DateTime.now().toSeconds() > session.expires_at) {
+    return res.redirect('/refresh-token');
+  }
+
+  try {
+    const headers = {
+      Authorization: `Bearer ${session.access_token}`,
+    };
+
+    const response = await axios.get(`${API_BASE_URL}me/playlists`, { headers });
+    const playlists = response.data;
+    address = playlists.items[0].href;
+    console.log('testing address', address)
+    return res.json(`<a href="${playlists.items[0].href}">playlist</a>`);
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to fetch playlists' });
+  }
+});
+
+app.get('/refresh-token', async (req, res) => {
+  if (!session.refresh_token) {
+    return res.redirect('/login');
+  }
+
+  if (DateTime.now().toSeconds() > session.expires_at) {
+    const reqBody = {
+      grant_type: 'refresh_token',
+      refresh_token: session.refresh_token,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+    };
+
+    try {
+      const response = await axios.post(TOKEN_URL, querystring.stringify(reqBody), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      });
+
+      const newTokenInfo = response.data;
+
+      session.access_token = newTokenInfo.access_token;
+      session.expires_at = DateTime.now().toSeconds() + newTokenInfo.expires_in;
+
+      return res.redirect('/playlists');
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to refresh token' });
+    }
+  }
+});
+
+const PORT = process.env.PORT || 8888;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+// ADDING SONG TO PLAYLIST
+const addTrackToPlaylist = async () => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${session.access_token}`,
+      'Content-Type': 'application/json',
+    };
+
+    const requestBody = {
+      uris: [`spotify:track:${trackId}`], // Assuming track ID is specified like this
+    };
+
+    const response = await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      requestBody,
+      { headers }
+    );
+
+    console.log('Track added to playlist:', response.data);
+  } catch (error) {
+
+    console.error('Failed to add track to playlist:', error.response.data);
+  }
+};
--- spotbot.js
+++ spotbot.js
@@ -1,12 +1,15 @@
-require('dotenv/config');
+import 'dotenv/config';
 // Will give us access to all the .env variables
-const axios = require('axios');
+ import axios from 'axios';
 // the axios library in Node.js to make HTTP requests
 
+let address = '';
+let songSuggestion = {};
 const clientId = 'b9187564d05d46e891a197b24dc20983';
 const clientSecret = process.env.SPOTKEY;
-
-const { Client } = require('discord.js');
+const trackId = '3nlGByvetDcS1uomAoiBmy';
+const playlistId = '3cKZDGGHg6r9pAiwIOXGpZ';
+import { Client } from 'discord.js';
 
 const client = new Client({
     intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
@@ -22,77 +25,33 @@ const CHANNELS = ['1182322735817441320']
 /////////
 // Spotify chatbot code:
 ////////
-const SpotifyWebAPI = require('spotify-web-api-node');
-const spotifyApi = new SpotifyWebAPI({
-  clientId: clientId,
-  clientSecret: clientSecret,
-  redirectUri: 'http://localhost:3000/',
-});
+// import SpotifyWebAPI from 'spotify-web-api-node';
+// const spotifyApi = new SpotifyWebAPI({
+//   clientId: clientId,
+//   clientSecret: clientSecret,
+//   redirectUri: 'http://localhost:3000/',
+// });
 
-// To get the access tokens to spotify API
-const getToken = async () => {
-    try {
-      const response = await axios.post('https://accounts.spotify.com/api/token', null, {
-        params: {
-          grant_type: 'client_credentials',
-        },
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        auth: {
-          username: clientId,
-          password: clientSecret,
-        },
-      });
-  
-      const { access_token, token_type, expires_in } = response.data;
-      console.log('Access Token:', access_token);
-      console.log('Token Type:', token_type);
-      console.log('Expires In:', expires_in);
-  
-      // Use the access token for making authenticated requests to the Spotify API
-      // ...
-  
-    } catch (error) {
-      console.error('Error obtaining access token:', error.message);
-    }
-  };
-  
-  // Call the function to obtain the access token
-  getToken();
-
-// Replace the following line with the actual access token you obtained
-const accessToken = 'BQC4umkgtUxIB4Y5jy_wIF6xbtV4IZA2S4rhEy5qLZN7Tnwm77uCleWYF5N7V_TTHDLMCYQkdw-8kwxB2Bibkby_hBIRFBEiWa18fmfjXjejKZKwW50';
-
-// Set the access token in the Spotify API object
-spotifyApi.setAccessToken(accessToken);
-
-// Code to check connection to spotify API by console logging an Elvis album info:
-spotifyApi.getAlbum('6oWz2hJ89n9mKarg3SO9ou').then(
-    function(data) {
-      console.log('Artist albums', data.body);
-    },
-    function(err) {
-      console.error(err);
-    }
-  );
-
+// 
 // Start of Chatbot Code
+// ///
 client.on('messageCreate', async (message) => {
     console.log(`Client heard message and should show content here: ${message.content}`);
     if (message.content.startsWith("!play")) {
-        console.log("!play happened")
+      // addTrackToPlaylist();
+        // console.log("!play happened")
+        // console.log('the address is a success!', address)
 
 
         var songname = message.content.replace('!play ', '');
         console.log(`Song name: ${songname}`);
 
-        spotifyApi.searchArtists(songname)
-            .then(function(data) {
-                console.log(`Search artists by ${songname}`, data.body);
-            }, function(err) {
-                console.error(err);
-            });
+        // spotifyApi.searchArtists(songname)
+        //     .then(function(data) {
+        //         console.log(`Search artists by ${songname}`, data.body);
+        //     }, function(err) {
+        //         console.error(err);
+        //     });
 
         // spotify.search({ type: 'track', query: songname, limit: 1 }, function(err, data) {
         //     console.log('Spotify API data:', data);
@@ -200,4 +159,227 @@ client.login(process.env.TOKEN);
 // });
 
 // client.login(process.env.TOKEN);
+
+// SPOTIFY APP ACCESS
+
+
+import express from 'express';
+import querystring from 'querystring';
+// import axios from 'axios';
+import { DateTime } from 'luxon';
+import 'dotenv/config';
+
+const app = express();
+app.use(express.json());
+
+const CLIENT_ID = '2c52abffbc8742d2b5441acdc4aec18d';
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = 'http://localhost:8888/callback';
+
+const AUTH_URL = 'https://accounts.spotify.com/authorize';
+const TOKEN_URL = 'https://accounts.spotify.com/api/token';
+const API_BASE_URL = 'https://api.spotify.com/v1/';
+
+let session = {};
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the Spotify app <a href='/login'>Login</a>");
+});
+
+app.get('/login', (req, res) => {
+  const scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+
+  const params = {
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    scope: scope,
+    redirect_uri: REDIRECT_URI,
+    show_dialog: true,
+  };
+
+  const authUrl = `${AUTH_URL}?${querystring.stringify(params)}`;
+
+  res.redirect(authUrl);
+});
+
+app.get('/callback', async (req, res) => {
+  if ('error' in req.query) {
+    return res.json({ error: req.query.error });
+  }
+
+  if ('code' in req.query) {
+    const reqBody = {
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      redirect_uri: REDIRECT_URI,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+    };
+
+    try {
+      const response = await axios.post(TOKEN_URL, querystring.stringify(reqBody), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      });
+
+      const tokenInfo = response.data;
+
+
+      // Store token information in session
+      session.access_token = tokenInfo.access_token;
+      session.refresh_token = tokenInfo.refresh_token;
+      session.expires_at = DateTime.now().toSeconds() + tokenInfo.expires_in;
+
+      // Log the token information and session after retrieval
+      // console.log('Token Info:', tokenInfo);
+      // console.log('Session after token retrieval:', session);
+
+      return res.redirect('/playlists');
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to retrieve tokens' });
+    }
+  }
+});
+
+app.get('/playlists', async (req, res) => {
+  // console.log('Session before accessing playlists:', session);
+  if (!session.access_token) {
+    return res.redirect('/login');
+  }
+
+  if (DateTime.now().toSeconds() > session.expires_at) {
+    return res.redirect('/refresh-token');
+  }
+
+  try {
+    const headers = {
+      Authorization: `Bearer ${session.access_token}`,
+    };
+
+    const response = await axios.get(`${API_BASE_URL}me/playlists`, { headers });
+    const playlists = response.data;
+    address = playlists.items[0].href;
+    console.log('testing address', address)
+    return res.json(`<a href="${playlists.items[0].href}">playlist</a>`);
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to fetch playlists' });
+  }
+});
+
+app.get('/refresh-token', async (req, res) => {
+  if (!session.refresh_token) {
+    return res.redirect('/login');
+  }
+
+  if (DateTime.now().toSeconds() > session.expires_at) {
+    const reqBody = {
+      grant_type: 'refresh_token',
+      refresh_token: session.refresh_token,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+    };
+
+    try {
+      const response = await axios.post(TOKEN_URL, querystring.stringify(reqBody), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      });
+
+      const newTokenInfo = response.data;
+
+      session.access_token = newTokenInfo.access_token;
+      session.expires_at = DateTime.now().toSeconds() + newTokenInfo.expires_in;
+
+      return res.redirect('/playlists');
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to refresh token' });
+    }
+  }
+});
+
+const PORT = process.env.PORT || 8888;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+// ADDING SONG TO PLAYLIST
+const addTrackToPlaylist = async () => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${session.access_token}`,
+      'Content-Type': 'application/json',
+    };
+
+    const requestBody = {
+      uris: [`spotify:track:${trackId}`], // Assuming track ID is specified like this
+    };
+
+    const response = await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      requestBody,
+      { headers }
+    );
+
+    console.log('Track added to playlist:', response.data);
+  } catch (error) {
+
+    console.error('Failed to add track to playlist:', error.response.data);
+  }
+};
+
+
+// export const testing = 'the test has passed';
+// export function test() {
+//     console.log('test passed', testing);
+// }
+
+/////
+// OLD SPOTIFY APP CODE
+/////
+// To get the access tokens to spotify API
+// const getToken = async () => {
+//     try {
+//       const response = await axios.post('https://accounts.spotify.com/api/token', null, {
+//         params: {
+//           grant_type: 'client_credentials',
+//         },
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//         auth: {
+//           username: clientId,
+//           password: clientSecret,
+//         },
+//       });
+  
+//       const { access_token, token_type, expires_in } = response.data;
+//       console.log('Access Token:', access_token);
+//       console.log('Token Type:', token_type);
+//       console.log('Expires In:', expires_in);
+  
+//       // Use the access token for making authenticated requests to the Spotify API
+//       // ...
+  
+//     } catch (error) {
+//       console.error('Error obtaining access token:', error.message);
+//     }
+//   };
+  
+//   // Call the function to obtain the access token
+//   getToken();
+
+// // Replace the following line with the actual access token you obtained
+// const accessToken = 'BQC4umkgtUxIB4Y5jy_wIF6xbtV4IZA2S4rhEy5qLZN7Tnwm77uCleWYF5N7V_TTHDLMCYQkdw-8kwxB2Bibkby_hBIRFBEiWa18fmfjXjejKZKwW50';
+
+// // Set the access token in the Spotify API object
+// spotifyApi.setAccessToken(accessToken);
+
+// // Code to check connection to spotify API by console logging an Elvis album info:
+// spotifyApi.getAlbum('6oWz2hJ89n9mKarg3SO9ou').then(
+//     function(data) {
+//       console.log('Artist albums', data.body);
+//     },
+//     function(err) {
+//       console.error(err);
+//     }
+//   );
 
